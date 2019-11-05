c194ed035d4a7f05c20bb30ecb3aadd6f9cf9e77
piotte13 <jeremie.piotte@gmail.com>
2019-11-05 18:49:41 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const csvToNdjson = require('csv-to-ndjson');
+var fs = require('fs');
+
+const args = process.argv.slice(2);
+const inFile = args[0];
+const outFile = args[1];
+
+fs.readFile(inFile, function(err, buf) {
+    let nbOfColumns = buf
+        .toString()
+        .split("\n")[0]
+        .match(/,/g)
+        .length;
+    let newString = buf
+        .toString()
+        .replace(/'/g, "\\'")
+        .replace(/"/g, '\\"')
+        .split("\n")
+        .map(line => {
+            let commaCount = (line.match(/,/g) || []).length;
+            if (commaCount < nbOfColumns)
+                for(let i = 0; i< (nbOfColumns - commaCount); i++)
+                    line += ","
+
+            line = line.split(",").map( e => {
+                if(isNaN(e))
+                    return '"' + e + '"'
+                else
+                    return e
+            }).join()
+            line = "[" + line + "]\n";
+            return line
+        });
+
+
+    console.log(newString[1]);
+
+    fs.writeFile(outFile, newString.join(''), function(err) {
+        if (err) throw err;
+    });
+});
